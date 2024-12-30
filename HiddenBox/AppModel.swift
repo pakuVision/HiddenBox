3ceacdc40ce1408d8c7c073246da5782c4768d06
pakuVision <boardguy.vision@gmail.com>
2024-12-30 17:40:09 +0900

    Initial Commit

---
+++ HiddenBox/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  HiddenBox
+//
+//  Created by boardguy.vision on 2024/12/30.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ HiddenBox/HiddenBoxApp.swift
@@ -0,0 +1,33 @@
+//
+//  HiddenBoxApp.swift
+//  HiddenBox
+//
+//  Created by boardguy.vision on 2024/12/30.
+//
+
+import SwiftUI
+
+@main
+struct HiddenBoxApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
